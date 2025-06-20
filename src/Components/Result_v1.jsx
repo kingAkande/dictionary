b26b1d06b5cca5a260034b125826cd5bf68e33ad
KingAkande <kingakande@gmail.com>
2025-06-20 01:47:12 +0100

    worked on the playbutton

--- src/Components/Result_v1.jsx
+++ src/Components/Result_v1.jsx
@@ -3,13 +3,13 @@ import playbutton from "../assets/PlayButton.png"
 
 const Result_v1 = ({word}) => {
   return (
-    <div className="w-[722px] h-[144px] border flex items-center justify-between">
+    <div className="w-[722px] h-[144px] flex items-center justify-between mb-6">
         <div>
 
-      <h1 className="font-bold font-serif text-7xl w-[293px] h-[77px] border ">{word}</h1>
+      <h1 className="font-bold font-serif text-7xl w-[293px] h-[77px]  ">{word}</h1>
       <p className="mt-2 font-sans text-3xl text-[#A445ED]">/ˈkiːbɔːd/</p>
         </div>
-      <div className="w-[73.47px] h-[75px] border rounded-full bg-[#ead7f8] relative ">
+      <div className="w-[73.47px] h-[75px] rounded-full bg-[#ead7f8] relative ">
         <img className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2  " src={playbutton} alt="play button" />
       </div>
     </div>
